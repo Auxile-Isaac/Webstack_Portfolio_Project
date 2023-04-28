0ea4e0a88074e7832e32649dd35629a9ca801a27
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-04-28 10:19:34 +0100

    Added the Middlewares directory

---
+++ server/middlewares/error.js
@@ -0,0 +1,24 @@
+// Handle specific errors
+export default  (err, req, res, next) => {
+
+    // set default error code/ set error code
+    err.statusCode = err.statusCode || 500;
+
+    // Handle development errors, show full stack trace
+    if(process.env.NODE_ENV === 'DEVELOPMENT') {
+        res.status(err.statusCode).json({
+            success: false,
+            error: err,
+            errorMessage: err.message,
+            errorStack: err.stack,
+        })
+    }
+
+    // handle Production errors
+    if(process.env.NODE_ENV === 'PRODUCTION') {
+        res.status(err.statusCode).json({
+            success: false,
+            error: err,
+        })
+    }
+}
