eeb55027ddbe559d83b26925c600435a0eda65f9
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-04-28 10:20:25 +0100

    Added the Models via upload

---
+++ server/models/appointmentModel.js
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+
+const { Schema, model } = mongoose;
+
+const AppointmentSchema = new Schema({
+
+    linkedto: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor",
+    },
+    appointmentDate: {
+        type: Date,
+    },
+    appointmentTime: [
+        {
+            start: {type: Date, required: true},
+            end: {type: Date, required: true}
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    appointmentType: {
+        type: String,
+        default: "Consultation",
+    },
+    notes: {
+        type: String,
+    },
+    status: {
+        type: String,
+        default: "active",
+    },
+    umail: {
+        type: String,
+        default: "",
+    },
+    dmail: {
+        type: String,
+        default: "",
+    },
+});
+
+// Validate the appointment start and end date
+// ensure start date is not in the past
+// AppointmentSchema.path('appointmentTime.start').validate({
+//     validator: function(value) {
+//         const currDate = new Date.now;
+//         return value > currDate;
+//     },
+//     message: 'Selected appointment start date cannot be a past date'
+//     });  
+
+// // ensure end date is greater than start
+// AppointmentSchema.path('appointmentTime.end').validate({
+//     validator: function(value) {
+//       return value > this.appointmentTime
+//       .start;
+//     },
+//     message: 'Selected appointment end date must be greater than start date'
+//   });
+
+  export default model("Appointment", AppointmentSchema);
---
+++ server/models/doctorModel.js
@@ -0,0 +1,78 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const doctorSchema = new Schema({
+
+    linkedto: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    name: { 
+        type: String,
+    },
+    phone: {
+        type: String,
+        minlength: [10, "Number must be at least 10 digits, do not include dialing code"],
+        maxlength: 15,
+        unique: true,
+        required: true,
+    },
+    email: {
+        type: String,
+    },
+    specialty: {
+        type: String,
+        default: "General Practice",
+    },
+    yearsExp: {
+        type: Number,
+        required: true,
+    },
+    availableHrs: {
+        type: Array,
+        default: [
+            {
+                "day_of_week": "Monday",
+                "start_time": "09:00",
+                "end_time": "03:00"
+            },
+            {
+                "day_of_week": "Tuesday",
+                "start_time": "09:00",
+                "end_time": "03:00"
+            },
+            {
+                "day_of_week": "Wednesday",
+                "start_time": "09:00",
+                "end_time": "03:00"
+            },
+            {
+                "day_of_week": "Thursday",
+                "start_time": "09:00",
+                "end_time": "03:00"
+            },
+            {
+                "day_of_week": "Friday",
+                "start_time": "09:00",
+                "end_time": "03:00"
+            },
+        ], 
+    },
+    consultFee: {
+        type: Number,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+});
+
+export default model("Doctor", doctorSchema);
